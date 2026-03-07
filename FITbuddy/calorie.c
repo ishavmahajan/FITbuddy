16e185135dbfae9174787a725fa5857d76e69f7f
Ravnoor-kaur <Rkaur50496@conestogac.on.ca>
2026-03-06 21:23:15 -0500

    Initial setup: struct, file load/save, test file

---
+++ FITbuddy/calorie.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "calorie.h"
+
+// Array to store entries in memory
+struct FoodEntry entries[MAX_ENTRIES];
+int entryCount = 0;
+
+// File names
+const char* CALORIE_FILE = "calories.txt";
+const char* GOAL_FILE = "goal.txt";
+
+// load entries from calories.txt
+void loadCaloriesFromFile()
+{
+    FILE* file = fopen(CALORIE_FILE, "r");
+    if (!file) return;  // File might not exist yet
+
+    entryCount = 0;
+    while (fscanf(file, "%10[^,],%49[^,],%d\n",
+        entries[entryCount].date,
+        entries[entryCount].foodName,
+        &entries[entryCount].calories) == 3)
+    {
+        entryCount++;
+        if (entryCount >= MAX_ENTRIES) break; // prevent overflow
+    }
+    fclose(file);
+}
+
+// save entries to file
+void saveCaloriesToFile()
+{
+    FILE* file = fopen(CALORIE_FILE, "w");
+    if (!file) return;
+
+    for (int i = 0; i < entryCount; i++)
+    {
+        fprintf(file, "%s,%s,%d\n",
+            entries[i].date,
+            entries[i].foodName,
+            entries[i].calories);
+    }
+    fclose(file);
+}
---
+++ FITbuddy/calorie.h
@@ -0,0 +1,33 @@
+#ifndef CALORIE_H
+#define CALORIE_H
+
+#define MAX_ENTRIES 500
+
+// Struct for one food entry
+struct FoodEntry
+{
+    char foodName[50];  // Name of the food
+    int calories;       // Calories of the food
+    char date[11];      // Date in YYYY-MM-DD format
+};
+
+// Function declarations
+//void calorieMenu();
+void addFoodEntry();  // allows to add the food 
+void viewTodayCalories();  // intake calories for today 
+void viewCaloriesByDate();  
+void deleteFoodEntry();
+void setCalorieGoal();
+void viewCalorieGoalStatus();
+
+// functions for the progress report integration 
+int getTodayCalories();    // Return total calories for today
+int getCaloriesByDate(const char* date);  // return total calories for a specific date YYYY-MM-DD
+int getWeeklyCalories();   // returns total calories in the last 7 days
+int getMonthlyCalories();   // returns total calories in the last 30 days
+
+// fiel handling 
+void loadCaloriesFromFile();
+void saveCaloriesToFile();
+
+#endif
