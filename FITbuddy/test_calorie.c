4ddaafe30c64133c2eb80e7e3d4fdf3a2ddca5cb
Ishav Mahajan <ishavm2800@gmail.com>
2026-03-25 10:37:25 -0400

    Created Unit Test Project

--- FITbuddy/test_calorie.c
+++ FITbuddy/test_calorie.c
@@ -1,9 +1,9 @@
-#include "calorie.h"
-
-int main()
-{
-    loadCaloriesFromFile();   // Load existing entries
-    //calorieMenu();            // Temporary menu for testing
-    saveCaloriesToFile();     // Save after test
-    return 0;
-}
+//#include "calorie.h"
+//
+//int main()
+//{
+//    loadCaloriesFromFile();   // Load existing entries
+//    //calorieMenu();            // Temporary menu for testing
+//    saveCaloriesToFile();     // Save after test
+//    return 0;
+//}
---
+++ TestProject/TestProject.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestProject
+{
+	TEST_CLASS(TestProject)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
